71e071a8058dca3d8f1a381dc47e2bd826721513
Abby Starnes <abbymstarnes@gmail.com>
2017-08-05 12:54:06 -0500

    tutorial complete

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,10 @@
 // function, nativescript-angular module, code to integrate NativeScript and Angular app version of platformBrowserDynamic()
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
-
+import { setStatusBarColors } from "./utils/status-bar-util";
 // typescript class .. both defined in seperate files
 import { AppModule } from "./app.module";
 
 // bootstrapModule expects an Angular module that contains the main configuration for your application,
 // in this case AppModule module defiend in app.module.ts
+setStatusBarColors();
 platformNativeScriptDynamic().bootstrapModule(AppModule);
