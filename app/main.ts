a79c4b19a5f6d390e911012a805e79f92ab349e3
Abby Starnes <abbymstarnes@gmail.com>
2017-08-05 10:47:53 -0500

    CH 3.4

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,9 @@
+// function, nativescript-angular module, code to integrate NativeScript and Angular app version of platformBrowserDynamic()
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 
+// typescript class .. both defined in seperate files
 import { AppModule } from "./app.module";
 
+// bootstrapModule expects an Angular module that contains the main configuration for your application,
+// in this case AppModule module defiend in app.module.ts
 platformNativeScriptDynamic().bootstrapModule(AppModule);
